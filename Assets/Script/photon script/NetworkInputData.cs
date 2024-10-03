c1f59fab7cd8930daa30c1c6f92c683f5f79f8d0
Andy567194 <www.andyfoo56@gmail.com>
2024-10-03 16:26:21 +0800

    fix

--- Assets/Script/photon script/NetworkInputData.cs
+++ Assets/Script/photon script/NetworkInputData.cs
@@ -1,16 +1,16 @@
 using Fusion;
 using UnityEngine;
-
+/*
 public enum InputButton
 {
     Jump,
-}
+}*/
 
 public struct NetworkInputData : INetworkInput
 {
-    public NetworkButtons Buttons;
-    public Vector2 Direction;
-    public Vector2 LookDelta;
+    //public NetworkButtons Buttons;
+    //public Vector2 Direction;
+    // public Vector2 LookDelta;
     public Vector3 direction;
 
 }
