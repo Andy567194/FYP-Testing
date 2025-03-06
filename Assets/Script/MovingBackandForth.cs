b26356660502632d47eaf33315c3f359af10c81e
Little-8usters <michae2681@gmail.com>
2025-03-06 14:12:22 +0800

    update2

---
+++ Assets/Script/MovingBackandForth.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBackandForth : MonoBehaviour
+{
+    public enum Axis { X, Y, Z }
+    public Axis moveAxis = Axis.X;
+    public float speed = 5f;
+
+    private Vector3 direction;
+
+    void Start()
+    {
+        // Set initial direction based on the selected axis
+        switch (moveAxis)
+        {
+            case Axis.X:
+                direction = Vector3.right;
+                Debug.Log("Moving along X axis");
+                break;
+            case Axis.Y:
+                direction = Vector3.up;
+                Debug.Log("Moving along Y axis");
+                break;
+            case Axis.Z:
+                direction = Vector3.forward;
+                Debug.Log("Moving along Z axis");
+                break;
+        }
+    }
+
+    void Update()
+    {
+        // Move the object along the specified axis
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Reverse the direction when a collision is detected
+        direction = -direction;
+    }
+}
